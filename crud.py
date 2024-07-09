f079817c322b247447a3f396ad4a5a25e17cb1d3
Julian Baumann <julian@baumanns-web.de>
2024-07-09 14:51:29 +0200

    Testing stuff

--- crud.py
+++ crud.py
@@ -99,6 +99,27 @@ async def get_withdraw_links(wallet_ids: Union[str, List[str]]) -> List[Withdraw
     )
     return [WithdrawLink(**row) for row in rows]
 
+async def get_withdraw_links_test(wallet_ids: List[str], limit: int, offset: int) -> Page[WithdrawLink]:
+    rows = await db.fetchall(
+        """
+        SELECT * FROM withdraw.withdraw_link
+        WHERE wallet IN ({})
+        ORDER BY id DESC
+        LIMIT ? OFFSET ?
+        """.format(','.join('?' * len(wallet_ids))),
+        (*wallet_ids, limit, offset)
+    )
+    
+    total = await db.fetchone(
+        """
+        SELECT COUNT(*) as total FROM withdraw.withdraw_link
+        WHERE wallet IN ({})
+        """.format(','.join('?' * len(wallet_ids))),
+        (*wallet_ids,)
+    )
+    
+    return Page(data=[WithdrawLink.parse_obj(dict(**row)) for row in rows], total=total['total'])
+
 
 async def remove_unique_withdraw_link(link: WithdrawLink, unique_hash: str) -> None:
     unique_links = [
